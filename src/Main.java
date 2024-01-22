c7614616e6b8bcf3f7ff8efc832b28be31a66d5c
JSaponaro <josephsaponaro2020@gmail.com>
2024-01-22 16:01:35 -0500

    1/22/2024

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+// Joseph Saponaro ObjODesign Project 1
+public class Main {
+    public static void main(String[] args) {
+        Simulator simulator = new Simulator();
+        simulator.runLongSimulation();
+    }
+}
